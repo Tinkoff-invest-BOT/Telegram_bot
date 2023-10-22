537c5ab9273f7faf14a4273b087b7148c372e9c5
Shamilier <98946822+Shamilier@users.noreply.github.com>
2023-10-23 00:26:27 +0300

    Delete Tinkoff_invest/smth.py

--- Tinkoff_invest/smth.py
+++
@@ -1,9 +0,0 @@
-with open('figis.txt', encoding='utf-8') as f:
-    t = f.readlines()
-    for i in t:
-        row = i.strip()
-        counter = 0
-        for letter in range(len(row)):
-            if str(row[letter]) == ' ' and counter == 0:
-                counter +=1
-                print(row[:letter+1] + '*' + row[letter+1: ])
