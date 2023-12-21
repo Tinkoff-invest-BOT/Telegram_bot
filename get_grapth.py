44a7c952a9c0925c62d9f7deb3b880890e917cc3
Shamilier <98946822+Shamilier@users.noreply.github.com>
2023-12-21 12:23:22 +0300

    Add files via upload

--- get_grapth.py
+++ get_grapth.py
@@ -5,11 +5,13 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 import plotly.graph_objects as go
 import apimoex
-from mt5_funcs import get_symbol_names, TIMEFRAMES, dict_timeframes
+from mt5_funcs import get_symbol_names, TIMEFRAMES, dict_timeframes, INDICATORS
+# import ta.trend as ta
+import pandas_ta as ta
+
 
 # creates the Dash App
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
 symbol_dropdown = html.Div([
     html.P('Symbol:'),
@@ -29,19 +31,31 @@ timeframe_dropdown = html.Div([
     )
 ])
 
+indicator_dropdown = html.Div([
+    html.P('Indicators:'),
+    dcc.Dropdown(
+        id='indicator-dropdown',
+        options=[{'label': indicator, 'value': indicator} for indicator in INDICATORS],
+        value='CLEAR'
+    )
+])
+
+
+
 num_bars_input = html.Div([
     html.P('Number of Candles'),
-    dbc.Input(id='num-bar-input', type='number', value='20')
+    dbc.Input(id='num-bar-input', type='number', value='50')
 ])
 
 # creates the layout of the App
-app.layout = html.Div([
+app.layout = html.Div(children=[
     html.H1('Real Time Charts'),
 
     dbc.Row([
         dbc.Col(symbol_dropdown),
         dbc.Col(timeframe_dropdown),
-        dbc.Col(num_bars_input)
+        dbc.Col(num_bars_input),
+        dbc.Col(indicator_dropdown)
     ]),
 
     html.Hr(),
@@ -50,43 +64,64 @@ app.layout = html.Div([
 
     html.Div(id='page-content')
 
-], style={'margin-left': '5%', 'margin-right': '5%', 'margin-top': '20px'})
+], style={'margin-left': '7%', 'margin-right': '5%', 'margin-top': '20px'})
 
 
 @app.callback(
     Output('page-content', 'children'),
     Input('update', 'n_intervals'),
-    State('symbol-dropdown', 'value'), State('timeframe-dropdown', 'value'), State('num-bar-input', 'value')
+    State('symbol-dropdown', 'value'), State('timeframe-dropdown', 'value'), State('num-bar-input', 'value'), State('indicator-dropdown', 'value')
 )
-def update_ohlc_chart(interval, symbol, timeframe, num_bars):
+def update_ohlc_chart(interval, symbol, timeframe, num_bars, indicator):
     timeframe_str = timeframe
     timeframe = dict_timeframes[timeframe]
     num_bars = int(num_bars)
     print(symbol, timeframe, num_bars, timeframe)
-    from_ = (datetime.datetime.now() - datetime.timedelta(weeks=100)).strftime("%Y-%m-%d")
+    from_ = (datetime.datetime.now() - datetime.timedelta(weeks=300)).strftime("%Y-%m-%d")
     till = datetime.datetime.now().strftime("%Y-%m-%d")
     query = f'http://iss.moex.com/iss/engines/stock/markets/shares/securities/{symbol}/candles.csv?iss.meta=on&iss.reverse=true&from={from_}&till={till}&interval={timeframe}'
     df = pd.read_csv(query, sep=';', header=1)
+    if indicator != "CLEAR":
+        indicator = indicator.lower()
+        df[indicator] = getattr(ta, indicator)(close = df.close, length=10)
     df = df.head(num_bars)
     df['end'] = pd.to_datetime(df['end'])
-    # df['open'] = df['open'].astype(float)
-    # df['high'] = df['high'].astype(float)
-    # df['low'] = df['low'].astype(float)
-    # df['close'] = df['close'].astype(float)
+
     fig = go.Figure(data=go.Candlestick(x=df['end'],
                     open=df['open'],
                     high=df['high'],
                     low=df['low'],
                     close=df['close']))
+                    # template = "plotly_dark",
+                    # title="Gapminder 2007: '%s' theme" % "plotly_dark"))
+    if indicator != "CLEAR":
+        fig.add_scatter(x=df['end'], y=df[indicator], mode='lines', line=dict(color='blue'), name=indicator.upper())
 
-    fig.update(layout_xaxis_rangeslider_visible=False)
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    # fig.update_layout(
+    #     plot_bgcolor='rgb(17,17,17)',
+    #     paper_bgcolor='rgb(10,10,10)')
+    fig.update_layout(plot_bgcolor = "black", paper_bgcolor='black',margin_l = 0, margin_t = 40)
+    fig.update_layout(width=1150, height= 700)
+    fig.update_xaxes(showgrid=False, gridwidth=0.001, gridcolor='gray', mirror=True, ticks='outside', showline=True)
+    fig.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', mirror=True, ticks='outside', showline=True)
+    # fig.update_layout(xaxis_showgrid=False, yaxis_showgrid=False)
     fig.update_layout(yaxis={'side': 'right'})
-    fig.layout.xaxis.fixedrange = True
-    fig.layout.yaxis.fixedrange = True
+    fig.update_layout(
+        newshape_line_color='white'
+    )
+    # fig.layout.xaxis.fixedrange = True
+    # fig.layout.yaxis.fixedrange = True
 
     return [
         html.H2(id='chart-details', children=f'{symbol} - {timeframe_str}'),
-        dcc.Graph(figure=fig, config={'displayModeBar': False})
+        dcc.Graph(figure=fig, config={'displaylogo': False, 'modeBarButtonsToAdd': ['drawline',
+                                        'drawopenpath',
+                                        'drawclosedpath',
+                                        'drawcircle',
+                                        'drawrect',
+                                        'eraseshape'
+                                       ]})
         ]
 
 
--- mt5_funcs.py
+++ mt5_funcs.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 TIMEFRAMES = ['M1', 'M10', 'H1', 'D1', 'W1', 'MN1']
+INDICATORS = ["CLEAR", "SMA", "EMA"]
 dict_timeframes = {
     'M1': 1,
     'M5': 10,
@@ -13,7 +14,11 @@ dict_timeframes = {
     'W1': 7,
     'MN1': 31,
 }
+
+
 def get_symbol_names():
     res = ['ABIO', 'ABRD', 'ACKO', 'AFKS', 'AFLT', 'AGRO', 'AKRN', 'ALRS', 'AMEZ', 'APTK', 'AQUA', 'ARSA', 'ASSB', 'ASTR', 'AVAN', 'BANE', 'BANEP', 'BELU', 'BISVP', 'BLNG', 'BRZL', 'BSPB', 'BSPBP', 'CARM', 'CBOM', 'CHGZ', 'CHKZ', 'CHMF', 'CHMK', 'CIAN', 'CNTL', 'CNTLP', 'DIOD', 'DSKY', 'DVEC', 'DZRD', 'DZRDP', 'EELT', 'ELFV', 'ENPG', 'ETLN', 'EUTR', 'FEES', 'FESH', 'FIVE', 'FIXP', 'FLOT', 'GAZA', 'GAZAP', 'GAZC', 'GAZP', 'GAZS', 'GAZT', 'GCHE', 'GECO', 'GEMA', 'GEMC', 'GLTR', 'GMKN', 'GTRK', 'HHRU', 'HIMCP', 'HMSG', 'HNFG', 'HYDR', 'IGST', 'IGSTP', 'INGR', 'IRAO', 'IRKT', 'JNOS', 'JNOSP', 'KAZT', 'KAZTP', 'KBSB', 'KCHE', 'KCHEP', 'KGKC', 'KGKCP', 'KLSB', 'KMAZ', 'KMEZ', 'KOGK', 'KRKN', 'KRKNP', 'KRKOP', 'KROT', 'KROTP', 'KRSB', 'KRSBP', 'KTSB', 'KTSBP', 'KUBE', 'KUZB', 'KZOS', 'KZOSP', 'LENT', 'LIFE', 'LKOH', 'LNZL', 'LNZLP', 'LSNG', 'LSNGP', 'LSRG', 'LVHK', 'MAGE', 'MAGEP', 'MAGN', 'MDMG', 'MFGS', 'MFGSP', 'MGNT', 'MGNZ', 'MGTS', 'MGTSP', 'MISB', 'MISBP', 'MOEX', 'MRKC', 'MRKK', 'MRKP', 'MRKS', 'MRKU', 'MRKV', 'MRKY', 'MRKZ', 'MRSB', 'MSNG', 'MSRS', 'MSTT', 'MTLR', 'MTLRP', 'MTSS', 'MVID', 'NAUK', 'NFAZ', 'NKHP', 'NKNC', 'NKNCP', 'NKSH', 'NLMK', 'NMTP', 'NNSB', 'NNSBP', 'NSVZ', 'NVTK', 'OGKB', 'OKEY', 'OMZZP', 'OZON', 'PAZA', 'PHOR', 'PIKK', 'PLZL', 'PMSB', 'PMSBP', 'POLY', 'POSI', 'PRFN', 'PRMB', 'QIWI', 'RASP', 'RBCM', 'RDRB', 'RENI', 'RGSS', 'RKKE', 'RNFT', 'ROLO', 'ROSB', 'ROSN', 'ROST', 'RTGZ', 'RTKM', 'RTKMP', 'RTSB', 'RTSBP', 'RUAL', 'RUSI', 'RZSB', 'SAGO', 'SAGOP', 'SARE', 'SAREP', 'SBER', 'SBERP', 'SELG', 'SFIN', 'SGZH', 'SIBN', 'SLEN', 'SMLT', 'SNGS', 'SNGSP', 'SOFL', 'SPBE', 'STSB', 'STSBP', 'SVAV', 'SVCB', 'SVET', 'TASB', 'TASBP', 'TATN', 'TATNP', 'TCSG', 'TGKA', 'TGKB', 'TGKBP', 'TGKN', 'TNSE', 'TORS', 'TORSP', 'TRMK', 'TRNFP', 'TTLK', 'TUZA', 'UGLD', 'UKUZ', 'UNAC', 'UNKL', 'UPRO', 'URKZ', 'USBN', 'UTAR', 'VEON-RX', 'VGSB', 'VGSBP', 'VJGZ', 'VJGZP', 'VKCO', 'VLHZ', 'VRSB', 'VRSBP', 'VSMO', 'VSYD', 'VSYDP', 'VTBR', 'WTCM', 'WTCMP', 'WUSH', 'YAKG', 'YKEN', 'YKENP', 'YNDX', 'YRSB', 'YRSBP', 'ZILL', 'ZVEZ']
     return res
+
+
 
