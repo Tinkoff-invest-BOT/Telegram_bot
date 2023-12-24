2580a593f0578eabef3831794130dd7c11b8d6cb
1balove <145463891+1balove@users.noreply.github.com>
2023-12-24 20:58:38 +0300

    analyzer completed

--- analyzer.py
+++ analyzer.py
@@ -2,13 +2,10 @@ import pandas as pd
 import numpy as np
 from pandas_datareader import data as pdr
 import yfinance as yfin
-from connection_db import connection
-from db import Database
 import datetime
-from tinkoff.invest import Client, CandleInterval
-from parser_daily import cast_money
 from passwords import *
-db = Database(connection)
+from functions import check_share_yahoo, check_share_moex
+from dateutil.parser import parse
 
 class Analyzer:
     def __init__(self, stocks_tickers: list, start_date : str, end_date : str, user_id: int):
@@ -18,26 +15,65 @@ class Analyzer:
         self.user_id = user_id
         self.data = pd.DataFrame()
         self.analysis = pd.DataFrame()
+        self.unable_to_parse_tickers = []
         
-        try:
-            self.parse_all()
-        except:
-            raise ValueError("Неправильное название акции")
+        self.__check_dates()
+        self.__parse_all()
+        
+    
+    def __check_dates(self):
+        for date in [self.start_date, self.end_date]:
+            try:
+                datetime.datetime.strptime(date, '%Y-%m-%d')
+            except:
+                raise ValueError("Неправильно введена дата.")
+    
     
     def text_analyser(self):
-        text = 'Результаты анализа:\n'
-        for i in self.analysis[self.analysis.index == 'sharpe'].columns():
-            text += i + ' - ' + self.analysis[self.analysis.index == 'sharpe'][i] + '\n'
-        return text
+        res = []
+        s = '\tРезультаты анализа:\n'
+        self.analysis = self.analysis.set_index('index')
+        for ticker in self.stocks_tickers:
+            s += f'<b>{ticker}</b><br>'
+            s += f'Cumulative return: {self.analysis.loc["cumulative"][ticker].round(2)}<br>'
+            s += f'Standard deviation: {self.analysis.loc["std"][ticker].round(2)}<br>'
+            s += f'Sharpe ratio: {self.analysis.loc["sharpe"][ticker].round(2)}<br>'
+        res.append(s)
+        if self.unable_to_parse_tickers:
+            s = "Не получилось собрать информацию с этих тикеров:"
+            for word in self.unable_to_parse_tickers:
+                s += ' ' + str(word)
+            res.append(s)
+        else:
+            res.append(None)
+        return res
             
     
-    def parse_all(self):
-        yahoo_tickers = np.setdiff1d(self.stocks_tickers, db.get_all_tinkoff_tickers())
-        tinkoff_tickers = np.setdiff1d(self.stocks_tickers, yahoo_tickers)
+    def __parse_all(self):
+        yahoo_tickers = []
+        moex_tickers = []
+        for ticker in self.stocks_tickers:
+            if check_share_yahoo(ticker):
+                yahoo_tickers.append(ticker)
+            elif check_share_moex(ticker):
+                moex_tickers.append(ticker)
+            else:
+                self.unable_to_parse_tickers.append(ticker)
+                
+        self.stocks_tickers = np.setdiff1d(self.stocks_tickers, self.unable_to_parse_tickers)
+        self.stocks_tickers = list(map(lambda x: x.upper(), self.stocks_tickers))
+        yahoo_tickers = list(map(lambda x: x.upper(), yahoo_tickers))
+        moex_tickers = list(map(lambda x: x.upper(), moex_tickers))
+
+        if yahoo_tickers:
+            self.__yahoo_stocks_parse(yahoo_tickers)
+        if moex_tickers:
+            self.__moex_stocks_parse(moex_tickers)
+        if moex_tickers or yahoo_tickers:
+            self.__get_overall_col_in_data()
+        if len(self.data) == 0:
+            raise ValueError("Не получилось собрать информацию ни по одному из введенных тикеров.")
         
-        self.__yahoo_stocks_parse(yahoo_tickers)
-        self.__tinkoff_stocks_parse(tinkoff_tickers)
-        self.__get_overall_col_in_data()
         
     def __yahoo_stocks_parse(self, stocks_tickers : list):
         # get not russian stocks
@@ -47,7 +83,6 @@ class Analyzer:
         if len(stocks_tickers) == 1:
             data = data.rename(stocks_tickers[0])
         data = data.reset_index()
-        data['Date'] = data['Date'].apply(lambda x: x.date())
         data = data.set_index('Date')
         if self.data.size == 0:
             self.data = data.fillna(method='bfill').fillna(method='ffill')
@@ -55,30 +90,17 @@ class Analyzer:
             self.data = self.data.join(data)
             self.data = self.data.fillna(method='bfill').fillna(method='ffill')
             
-    def __tinkoff_stocks_parse(self, stocks_tickers : list):
-        TOKEN = db.get_token(user_id=self.user_id)
-        if TOKEN == None:
-            TOKEN = TOKEN_SH
+            
+    def __moex_stocks_parse(self, stocks_tickers : list):
         data = pd.DataFrame()
         
-        for name in stocks_tickers:
-            figi = db.get_figi(name)
-            with Client(TOKEN) as client:
-                r = client.market_data.get_candles(
-                    figi=figi,
-                    from_=datetime.datetime(self.start_date) + datetime.timedelta(days=1),
-                    to=datetime.datetime(self.end_date) + datetime.timedelta(days=1),
-                    interval=CandleInterval.CANDLE_INTERVAL_DAY
-                )
-                
-                df = pd.DataFrame([{
-                    'time': c.time,
-                    'close': cast_money(c.close)
-                } for c in r.candles])
-                
-                data[name] = df['close']
-                data['Date'] = df['time'].apply(lambda x: x.date())
-        data = data.set_index('Date')
+        for ticker in stocks_tickers:
+            query = f'http://iss.moex.com/iss/engines/stock/markets/shares/securities/{ticker}/candles.csv?from={self.start_date}&till={self.end_date}&interval=24'
+            df = pd.read_csv(query, sep=';', header=1)
+            df['Date'] = df['begin'].apply(lambda x: parse(x))
+            df = df.set_index('Date')['close']
+            data[ticker] = df
+            
         if self.data.size == 0:
             self.data = data.fillna(method='bfill').fillna(method='ffill')
         else:
@@ -86,9 +108,9 @@ class Analyzer:
             self.data = self.data.fillna(method='bfill').fillna(method='ffill')
                 
         
-    
     def __get_overall_col_in_data(self):
         self.data['Overall'] = self.data.apply(lambda x: sum(x[name] for name in self.stocks_tickers), axis=1)
+        
         
     def sharpe_ratio(self, rfr: float):
         # self.analysis = DataFrame: cols - stocks, row - sharpe ratio
@@ -105,4 +127,12 @@ class Analyzer:
         indexes = np.setdiff1d(analysis['index'], self.analysis['index'])
         for i in indexes:
             self.analysis = pd.concat([self.analysis, analysis[analysis['index'] == i]])
-        
+            
+            
+a = Analyzer(['sber', 'aapl', 'as9asd9a'], '2021-01-01', '2022-12-01', 1231)
+a.sharpe_ratio(0.02)
+print(a.text_analyser()[1])
+
+
+
+
