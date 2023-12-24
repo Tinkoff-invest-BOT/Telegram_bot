05aa11d507fe2e6b3cf143ea186cf6126325c458
Shamilier <98946822+Shamilier@users.noreply.github.com>
2023-12-24 23:09:16 +0300

    Update my_get_operation.py

--- my_get_operation.py
+++ my_get_operation.py
@@ -8,6 +8,7 @@ from tinkoff.invest import Client, RequestError, PositionsResponse, AccessLevel,
     OperationState, OperationType
 from tinkoff.invest.services import Services
 
+
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
